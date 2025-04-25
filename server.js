f7d24f691fe8160949064269824cbd1a1dac4058
Shubham Vishwakarma <127945668+Shivamvish26@users.noreply.github.com>
2025-04-25 17:21:41 +0530

    conection done

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
-const express = require('express');
+const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
 
-app.get('/',(req,resp)=>{
-    resp.send('Api is working')
-})
+const connectDB = async () => {
+  mongoose.connect("mongodb://localhost:27017/e-commerce");
+  const productSchema = new mongoose.Schema({});
+  const product = mongoose.model("shop", productSchema);
+  const data = await product.find();
+  console.log(data);
+};
+connectDB();
 
-app.listen(5000,()=>{
-    console.log('Server is running on port 3000')
-})
+app.listen(5000, () => {
+  console.log("Server is running on port 5000");
+});
