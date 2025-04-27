8ce7521d65a0b1359e5cd15500008174a9f55be7
Shubham Vishwakarma <127945668+Shub26vish@users.noreply.github.com>
2025-04-27 18:05:01 +0530

    signup backend coded added

--- db/config.js
+++ db/config.js
@@ -3,3 +3,7 @@ const mongoose = require("mongoose");
 mongoose.connect("mongodb://127.0.0.1:27017/e-commerce")
   .then(() => console.log("MongoDB Connected Successfully"))
   .catch((error) => console.error("MongoDB Connection Failed:", error));
+
+// mongoose.connect("mongodb://127.0.0.1:27017/e-commerce")
+//   .then(() => console.log("MongoDB Connected Successfully"))
+//   .catch((error) => console.error("MongoDB Connection Failed:", error));
