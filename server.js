a390bfb4345b1aeb6b74e4614801d9cba1692723
Shubham Vishwakarma <127945668+Shivamvish26@users.noreply.github.com>
2025-04-28 18:02:06 +0530

    api code added

--- server.js
+++ server.js
@@ -1,19 +1,34 @@
 const express = require("express");
 const app = express();
-const User = require("./db/users")
-const cors = require("cors")
+const User = require("./db/users");
+const cors = require("cors");
 
-app.use(express.json())
-app.use(cors())
+app.use(express.json());
+app.use(cors());
 
-require("./db/config")
+require("./db/config");
 
 app.post("/register", async (req, resp) => {
-    let user = new User(req.body);
-    let result = await user.save();
-    resp.send(result)
-})
+  let user = new User(req.body);
+  let result = await user.save();
+  result = result.toObject();
+  delete result.password;
+  resp.send(result);
+});
+
+app.post("/login", async (req, resp) => {
+  if (req.body.password && req.body.email) {
+    let user = await User.findOne(req.body).select("-password");
+    if (user) {
+      resp.send(user);
+    } else {
+      resp.send({ result: "No User Found" });
+    }
+  } else {
+    resp.send({ result: "No User Found" });
+  }
+});
 
 app.listen(5000, () => {
-    console.log("Server is running on port 5000");
+  console.log("Server is running on port 5000");
 });
