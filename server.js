956d48fc41f5abd483d8d8168fc6f0bec689f4aa
Shubham Vishwakarma <127945668+Shivamvish26@users.noreply.github.com>
2025-04-29 18:20:37 +0530

    product code added

--- server.js
+++ server.js
@@ -1,13 +1,17 @@
 const express = require("express");
 const app = express();
 const User = require("./db/users");
+const Product = require("./db/products");
 const cors = require("cors");
+const multer = require("multer");
+const path = require("path");
 
 app.use(express.json());
 app.use(cors());
 
 require("./db/config");
 
+// register api
 app.post("/register", async (req, resp) => {
   let user = new User(req.body);
   let result = await user.save();
@@ -16,17 +20,25 @@ app.post("/register", async (req, resp) => {
   resp.send(result);
 });
 
+// login api
 app.post("/login", async (req, resp) => {
   if (req.body.password && req.body.email) {
     let user = await User.findOne(req.body).select("-password");
     if (user) {
-      resp.send(user);
+      resp.send({ status: "ok", data: user });
     } else {
-      resp.send({ result: "No User Found" });
+      resp.send({ status: "error", message: "No User Found" });
     }
   } else {
-    resp.send({ result: "No User Found" });
+    resp.send({ status: "error", message: "Email and Password are required" });
   }
+});
+
+// add product api
+app.post("/add-product", async (req, resp) => {
+  let product = new Product(req.body);
+  let result = await product.save();
+  resp.send(result);
 });
 
 app.listen(5000, () => {
